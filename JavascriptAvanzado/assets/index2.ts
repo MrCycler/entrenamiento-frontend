52094073966c45c0885378452b0a7d1f9048e9d0
Juan Manuel Mendoza <mendoza.jm@pucp.pe>
2020-03-29 19:25:11 -0500

    C1) proyecto completado agregado anuncios usando el patron singleton

--- JavascriptAvanzado/assets/index2.ts
+++ JavascriptAvanzado/assets/index2.ts
@@ -1,13 +1,14 @@
 import MediaPlayer from './MediaPlayer2';
 import AutoPlay from './plugins_ts/Autoplay';
 import AutoPause from './plugins_ts/AutoPause';
+import Ads from './plugins_ts/Ads';
 
 const video = document.querySelector("video");
 const playbutton : HTMLElement = document.getElementById("playButton");
 const mutebutton : HTMLElement = document.getElementById("muteButton");
 
 //Instancia
-const player = new MediaPlayer({ el: video, plugins:[new AutoPlay, new AutoPause] });
+const player = new MediaPlayer({ el: video, plugins:[new AutoPlay(), new AutoPause(),new Ads()] });
 //Usando el metodo
 playbutton.onclick = () => player.togglePlay();
 mutebutton.onclick = () => player.toggleSoud();
