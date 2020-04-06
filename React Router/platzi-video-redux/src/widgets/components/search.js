2160842d442051b3bf33b0938666e3974c8ad196
Juan Manuel Mendoza <mendoza.jm@pucp.pe>
2020-04-06 11:07:23 -0500

    c2) agregado un prompt

--- React Router/platzi-video-redux/src/widgets/components/search.js
+++ React Router/platzi-video-redux/src/widgets/components/search.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './search.css';
+import {Prompt} from 'react-router';
 // function Search(props) {
 //   return (
 //     <form action=""></form>
@@ -11,6 +12,10 @@ const Search = (props) => (
     className="Search"
     onSubmit={props.handleSubmit}
   >
+    <Prompt
+    when = {props.prompt}
+    message = "¿Voce esta segurinho?"
+    />
     <input
       ref={props.setRef}
       type="text"
