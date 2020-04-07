85c8d8d9c390d3a16c69ddb9d25a1b6c49b79689
Juan Manuel Mendoza <mendoza.jm@pucp.pe>
2020-04-06 22:46:19 -0500

    C2) modificado para el uso de la app desde express

---
+++ React Router/platzi-video-redux/src/pages/containers/app.js
@@ -0,0 +1,59 @@
+//Solo para el SSR
+import React,{Component} from 'react';
+
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+
+import { Map as map } from 'immutable';
+import logger from 'redux-logger';
+import thunk from 'redux-thunk';
+
+import { composeWithDevTools } from 'redux-devtools-extension';
+
+import {Route, Switch, Redirect} from 'react-router-dom';
+
+import Videos from './videos';
+import Home from '../components/home';
+import reducer from '../../reducers/index';
+import NotFound from '../components/not-found';
+import Header from '../components/header';
+
+const logger_ = ({getState, dispatch }) => next => action => {
+  console.log('este es mi viejo estado', getState().toJS())
+  console.log('vamos a enviar está acción', action);
+  const value = next(action)
+  console.log('este es mi nuevo estado', getState().toJS())
+  return value
+}
+
+const store = createStore(
+  reducer,
+  map(),
+  composeWithDevTools(
+    applyMiddleware(
+      logger,
+      thunk
+    )
+  )
+);
+
+class App extends Component{
+
+render(){
+    return(
+        <Provider store={store}>
+            <React.Fragment>
+            <Header />
+            <Switch>
+            <Route exact path="/" component={Home}/>
+            <Route exact path="/videos" component={Videos}></Route>
+            <Redirect from="/v" to="/videos" />
+            <Route component={NotFound}/>
+            </Switch>
+            </React.Fragment>
+            </Provider>
+    )
+}
+}
+
+export default App;
